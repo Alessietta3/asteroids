e83ce31e1f2589f2a050601c003a331eb0208582
ThePrimeagen <the.primeagen@aol.com>
2026-04-16 06:25:32 +0200

    CH 3: L 2: drawing player

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import pygame
 from logger import log_state
+from circleshape import CircleShape
+from player import  Player 
 from constants import SCREEN_WIDTH , SCREEN_HEIGHT 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    
+    dt =0
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    game_clock=pygame.time.Clock()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player=Player( x , y )
+
+
+
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             pass
         screen.fill("black")
+        dt=game_clock.tick(60)/1000
+        player.draw(screen)
+
+
+        
+
         pygame.display.flip()
 
 
